f21ef52c15c5e062c41cf701712a3fa92d4fc61d
Kreeptonium <bhushan.infinity@gmail.com>
2022-07-13 17:45:49 +0530

    commit

---
+++ src/CheckBalance.ts
@@ -0,0 +1,23 @@
+import Web3 from "web3";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const checkBalance = async():Promise<any>=>{
+try {
+    
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.rpcRinkebyURL));
+const addressBalance = await web3.eth.getBalance(process.env.walletAddress);
+console.log("Address Balance:",addressBalance);
+
+} catch (error) {
+    
+}
+
+    
+}
+checkBalance().catch((error)=>{
+console.log("Error: ", error);
+process.exitCode=1;
+})
+
